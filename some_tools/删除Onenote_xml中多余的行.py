e1cc6d1d5063fb4b96b273bc96beaad480896934
iKexinLL <kongpahuixiao@sina.com>
2017-04-24 20:19:23 +0800

    更新了Onenote_xml的删除规则

--- some_tools/删除Onenote_xml中多余的行.py
+++ some_tools/删除Onenote_xml中多余的行.py
@@ -6,7 +6,7 @@
 
 from bs4 import BeautifulSoup as bsp
 
-path_xml = r'E:\code\python\未完成\Untitled-1.xml'
+path_xml = r"E:\temp\201606.txt"
 
 # 读取xml文件
 with open(path_xml, encoding ='utf-8') as f:
@@ -21,8 +21,9 @@ the_new_line = []
 
 for conts in oe_children.find_all('OE'):
     temp_cont = conts.T
-    if temp_cont:
-        if temp_cont.text != '\n':
+    if temp_cont: 
+        # 修改='\n'为 '<br>\n' 感觉正则更好一点,可以匹配更多的条件
+        if '<br>\n' not in temp_cont.text:
             the_contents.append(conts)
         else:
             the_new_line.append(conts)
@@ -30,6 +31,8 @@ for conts in oe_children.find_all('OE'):
 
 for i in the_new_line:
     i.decompose()
+
+res = soup.prettify()
 
 with open(r'e:\temp\res_one.xml', 'w', encoding = 'utf-8') as f:
     f.write(res)    
