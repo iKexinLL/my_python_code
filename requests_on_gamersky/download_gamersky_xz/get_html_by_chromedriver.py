8c874bb0a0ace200079984c69ff6273a6e7ef3b3
kongp <kongpahuixiao@sina.com>
2018-07-18 15:03:15 +0800

    修改了"__handle_page"方法中的逻辑 已保证页面可以加载完毕后获取信息 并确保页面可以在未加载"下一页"时,可以退出并获取信息

--- requests_on_gamersky/download_gamersky_xz/get_html_by_chromedriver.py
+++ requests_on_gamersky/download_gamersky_xz/get_html_by_chromedriver.py
@@ -9,10 +9,10 @@ __desc__ = 使用chromedriver+selenium来爬取网站
 """
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class SpiderGamersky:
@@ -28,15 +28,20 @@ class SpiderGamersky:
 
     def __handle_page(self, download_page=0):
         """
+        经过一些尝试,发现在获取page_num的时候报错:StaleElementReferenceException
+        试过一些办法,发现忽略这个问题是最简单的~
+        所以直接利用循环增长页面
         :param download_page: 如果传入的数字为0,则将其设置为一个很大的数字~
         :return:
         """
-        # 加载首页
-        self.__get_forthcoming_urls()
-        download_page = 999999 if download_page == 0 else download_page
 
+        download_page = 999999 if download_page == 0 else download_page
+        page_now = 1
+
+        # 判断数据是否加载完毕
+        locator_ul = (By.CLASS_NAME, 'pictxt,contentpaging')
         # 传入特定的locator格式,判断下一页是否存在
-        locator_next_page = (By.LINK_TEXT, '下一页')
+        locator_page = (By.LINK_TEXT, '下一页')
         # 传入特定的locator格式,判断要查询的元素是否加载完毕
         # 不能用这个判断,貌似这个只会检验第一个con而不是检验最后一个con
         # (毕竟程序也不知道哪个是最后的con)
@@ -47,43 +52,31 @@ class SpiderGamersky:
         # 循环点击下一页,直到没有下一页为止
         # 需要等待数据完全载入后判断
         while 1:
+            # 这个是判断当前页面是否加载完毕
+            element_ul = WebDriverWait(self._browser, 10).until(
+                EC.presence_of_element_located(locator_ul)
+            )
+
             try:
-                # 这个是判断当前页面是否加载完毕
-                element_next_page = WebDriverWait(self._browser, 10).until(
-                    EC.presence_of_element_located(locator_next_page)
+                element_page = WebDriverWait(self._browser, 10).until(
+                    EC.presence_of_element_located(locator_page)
                 )
+            except TimeoutException:
+                print('无"下一页"元素, 停止循环, page: ' + str(page_now))
+                break
 
-                page_now = int(self._browser.find_element_by_class_name('p3').text)
+            # self.__get_forthcoming_urls()
 
-                if element_next_page and page_now < download_page:
-                    # next_page_button = self._browser.find_element_by_partial_link_text('下一页')
-                    self._browser.find_element_by_link_text('下一页').click()
+            if element_ul and element_page and page_now < download_page:
+                # next_page_button = self._browser.find_element_by_partial_link_text('下一页')
+                self._browser.find_element_by_link_text('下一页').click()
+                page_now += 1
+                # 不能用这个,理由见上面
+                # element_locator_con = WebDriverWait(self._browser, 10).until(
+                #     EC.presence_of_element_located(locator_con)
+                # )
 
-                    # 不能用这个,理由见上面
-                    # element_locator_con = WebDriverWait(self._browser, 10).until(
-                    #     EC.presence_of_element_located(locator_con)
-                    # )
-
-                    # 这个是判断点击'下一页'后,页面是否加载完毕
-                    try:
-                        element_next_page = WebDriverWait(self._browser, 10).until(
-                            EC.presence_of_element_located(locator_next_page)
-                        )
-
-                        if element_next_page:
-                            print('点击完下一页后的页数:' + self._browser.find_element_by_class_name('p3').text)
-                            self.__get_forthcoming_urls()
-
-                    except:
-                        pass
-
-                    if element_next_page:
-                        print('点击完下一页后的页数:' + self._browser.find_element_by_class_name('p3').text)
-                        self.__get_forthcoming_urls()
-                else:
-                    break
-
-            except NoSuchElementException:
+            else:
                 break
 
         return self._forthcoming_urls
@@ -110,4 +103,4 @@ class SpiderGamersky:
 
 
 if __name__ == '__main__':
-    SpiderGamersky.get_all_forthcoming_urls(2)
+    SpiderGamersky.get_all_forthcoming_urls(0)
