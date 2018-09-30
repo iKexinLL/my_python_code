a4e7f7d755a7f55f8d4856f5d870bf3f6c032c4d
kongp <kongpahuixiao@sina.com>
2018-09-30 08:54:14 +0800

    None

--- temp/some_code.py
+++ temp/some_code.py
@@ -1,32 +1,4 @@
-
-
-
-# if len(sys.argv == 1 or sys.argv[1] in {'-h','--help'})
-# 程序的工作目录(work path) os.getcwd() 
-# 更改工作目录(work path) os.chdir()
-# os.walk() 遍历, os.listdir()不会
-
-import sys 
-import test1
-
-def test():
-    test1.test_1_init()
-
-
-def main():
-    if len(sys.argv) == 1 or sys.argv[1] in {"-h","--help"}:
-        print('usage :{0} [en|fr] number'.format(sys.argv[0]))
-        sys.exit()
-
-    args = sys.argv[1:]
-    if args[0] in {'en','fr'}:
-        global Language
-        Language = args.pop(0)
-        print(args)
-        print('Language is ' + Language)
-
-    print('after ' + Language)
-
-
+#encoding:utf-8
 print("请问")
-test()
+
+
